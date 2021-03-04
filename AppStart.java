3b257633b2f1c78b56c051c5d0a824deced1894f
João Afonso <200221014@estudantes.ips.pt>
2021-03-04 17:43:44 +0000

    Second Commit

---
+++ AppStart.java
@@ -0,0 +1,13 @@
+
+/**
+ * Escreva a descrição da classe AppStart aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class AppStart
+{ 
+    public void main (String[] args){     
+        
+}
+}
